ddb0f97c36c2cd6ad3b0d987a16d449d8c77ea53
isvakulenko <ivan-vakulenko@yandex.ru>
2022-12-21 20:36:49 +0300

    Delete createUser router

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import {
   getUsers,
   getUserById,
-  createUser,
   editProfile,
   editAvatar,
 } from '../controllers/users';
@@ -13,8 +12,6 @@ const router = Router();
 router.get('/:userId', getUserById);
 // возвращает всех пользователей
 router.get('/', getUsers);
-// создаёт пользователя
-router.post('/', createUser);
 // обновляет профиль
 router.patch('/me', editProfile);
 // обновляет аватар
