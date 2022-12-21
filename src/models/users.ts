b25548476f92eaff5bd3bba873eed2952786c4ea
isvakulenko <ivan-vakulenko@yandex.ru>
2022-12-21 20:30:41 +0300

    Add findUserByCredentials

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,5 +1,6 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema, Document, Model } from 'mongoose';
 import { URLCheck, EMailCheck } from '../utils/const';
+import bcrypt from 'bcryptjs';
 
 interface IUser {
   name: string;
@@ -9,18 +10,25 @@ interface IUser {
   password: string;
 }
 
-const userSchema = new Schema<IUser>({
+interface UserModel extends Model<IUser> {
+  findUserByCredentials: (
+    email: string,
+    password: string
+  ) => Promise<Document<unknown, any, IUser>>;
+}
+
+const userSchema = new Schema<IUser, UserModel>({
   name: {
     // у пользователя есть имя — опишем требования к имени в схеме:
     type: String, // имя — это строка
     minlength: 2, //Custom Error Messages?
     maxlength: 30,
-    default: 'Жак-Ив Кусто'
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String, // информация о пользователе
     maxlength: 200,
-    default: 'Исследователь'
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
@@ -31,7 +39,8 @@ const userSchema = new Schema<IUser>({
       },
       message: 'Неправильный формат  URL', // выводится в случае false
     },
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'
+    default:
+      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
@@ -46,9 +55,35 @@ const userSchema = new Schema<IUser>({
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 8,
   },
 });
 
+// Собственные методы моделей Mongoose.
+// добавим метод findUserByCredentials схеме пользователя
+// у него будет два параметра — почта и пароль
+userSchema.static(
+  'findUserByCredentials',
+  function findUserByCredentials(email: string, password: string) {
+    // Чтобы найти пользователя по почте, нам потребуется метод findOne, которому передадим на вход email.
+    // Метод findOne принадлежит модели User, поэтому обратимся к нему через ключевое слово this:
+    return this.findOne({ email }) // this — это модель user
+      .then((user) => {
+        // не нашёлся — отклоняем промис
+        if (!user) {
+          return Promise.reject(new Error('Неправильные почта или пароль'));
+        }
+        // нашёлся — сравниваем хеши
+        return bcrypt.compare(password, user.password).then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user; // теперь user доступен
+        });
+      });
+  }
+);
+
 // TS-интерфейс модели User
-export default model<IUser>('user', userSchema);
+export default model<IUser, UserModel>('user', userSchema);
