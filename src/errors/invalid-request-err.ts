ca5dcf82f0b4c106e2e6131f3f27b93b04772479
isvakulenko <ivan-vakulenko@yandex.ru>
2022-12-24 11:03:39 +0300

    Add Classes for Errors 400, 401, 404

---
+++ src/errors/invalid-request-err.ts
@@ -0,0 +1,10 @@
+class InvalidRequestError extends Error {
+  statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+export default InvalidRequestError;
