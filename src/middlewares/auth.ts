fbd2b7dc72e145967bc9a11e2fc23e42d7977ebf
isvakulenko <ivan-vakulenko@yandex.ru>
2022-12-25 19:18:55 +0300

    Refactor errors handler

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import { Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { SessionRequest } from '../utils/types';
+import UnauthorizedError from '../errors/unauthorized-error';
 
 const { JWT_SECRET = 'some-secret-key' } = process.env;
 
@@ -9,7 +10,8 @@ export default (req: SessionRequest, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
   // убеждаемся, что он есть или начинается с Bearer
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
 
   // извлечём токен
@@ -21,7 +23,8 @@ export default (req: SessionRequest, res: Response, next: NextFunction) => {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     // отправим ошибку, если не получилось
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   } // записываем пейлоуд в объект запроса
   // его можно использовать в обработчиках.
   req.user = payload;
