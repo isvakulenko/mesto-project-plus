d261b8bbfb038c2bea4c8bfd7090c9915709c473
isvakulenko <ivan-vakulenko@yandex.ru>
2022-12-25 12:43:46 +0300

    Add Classes for Errors 403,409

---
+++ src/errors/duplicate-err.ts
@@ -0,0 +1,10 @@
+class DuplicateError extends Error {
+  statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = 409;
+  }
+}
+
+export default DuplicateError;
